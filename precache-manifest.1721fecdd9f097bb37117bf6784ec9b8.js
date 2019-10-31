51c96c5b8318fa03f2229edea6e68fd07650a48a
unknown <mech.pkkaushik@gmail.com>
2019-10-31 22:37:24 +0530

    Updates

---
+++ precache-manifest.1721fecdd9f097bb37117bf6784ec9b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200768b7ef1fe921c91c89ad571a9b4c",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "8646ef3eac842b049f8c",
+    "url": "/tictactoe/static/css/main.79b27199.chunk.css"
+  },
+  {
+    "revision": "5e4948c05cfbd34958dd",
+    "url": "/tictactoe/static/js/2.eedbcb71.chunk.js"
+  },
+  {
+    "revision": "8646ef3eac842b049f8c",
+    "url": "/tictactoe/static/js/main.8d88ec73.chunk.js"
+  },
+  {
+    "revision": "0286afb91386679c3c8a",
+    "url": "/tictactoe/static/js/runtime-main.28ad4404.js"
+  }
+]);
